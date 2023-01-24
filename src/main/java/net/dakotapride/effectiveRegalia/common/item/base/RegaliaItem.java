7046f3bce278978ce94abb6eddf79c2f8cd2b6f4
DakotaPride <rjtube13@gmail.com>
2023-01-24 09:09:33 -0600

    v0.2.0 - Beta Testing 'Durability Conditions'

--- src/main/java/net/dakotapride/effectiveRegalia/common/item/base/RegaliaItem.java
+++ src/main/java/net/dakotapride/effectiveRegalia/common/item/base/RegaliaItem.java
@@ -2,6 +2,10 @@ package net.dakotapride.effectiveRegalia.common.item.base;
 
 import net.dakotapride.effectiveRegalia.common.register.Constants;
 import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
@@ -16,6 +20,46 @@ public class RegaliaItem extends Item implements Constants {
 
     public RegaliaItem(Settings settings) {
         super(settings);
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+        if (entity instanceof PlayerEntity playerEntity) {
+            if (playerEntity.getOffHandStack().isOf(this.getDefaultStack().getItem())) {
+                if (stack.isIn(ironRegaliaItemsTag)) {
+                    if (stack.getDamage() < 615) {
+                        playerEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.WEAKNESS, 200, 0));
+                    }
+
+                    if (stack.getDamage() < 256) {
+                        playerEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.WEAKNESS, 200, 1));
+                        playerEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 200, 0));
+                    }
+                }
+
+                if (stack.isIn(goldenRegaliaItemsTag)) {
+                    if (stack.getDamage() < 1006) {
+                        playerEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.WEAKNESS, 200, 0));
+                    }
+
+                    if (stack.getDamage() < 402) {
+                        playerEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.WEAKNESS, 200, 1));
+                        playerEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 200, 0));
+                    }
+                }
+
+                if (stack.isIn(netheritePlatedRegaliaItemsTag)) {
+                    if (stack.getDamage() < 1358) {
+                        playerEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.WEAKNESS, 200, 0));
+                    }
+
+                    if (stack.getDamage() < 452) {
+                        playerEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.WEAKNESS, 200, 1));
+                        playerEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 200, 0));
+                    }
+                }
+            }
+        }
     }
 
     @Override
